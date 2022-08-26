e8d1b2903f96fbe662caa914176098b38e01fcdb
Korni Arjun Reddy <arjunreddykorni@gmail.com>
2022-08-26 16:04:35 +0530

    Writen the logic to read from a file useing FileInputStream.

--- src/main/java/com/jap/ballad/Ballad.java
+++ src/main/java/com/jap/ballad/Ballad.java
@@ -1,14 +1,44 @@
 package com.jap.ballad;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Ballad {
     public String readPoemFromAFile(String fileName){
        // Write the logic to read from a file
-        return "";
+        String response = "";
+
+        FileInputStream fileInputStream = null;
+        int data =0;
+
+        try{
+            fileInputStream = new FileInputStream(fileName);
+            while(data != 0){
+                data = fileInputStream.read();
+               char  dataChar = (char)data;
+               response = response + dataChar;
+
+            }
+
+        }catch (FileNotFoundException e){
+            System.out.println("e = " + e);
+            e.printStackTrace();
+
+        }catch (IOException e){
+            System.out.println("e = " + e);
+            e.printStackTrace();
+        }
+        return response;
     }
+
+    public static void main(String[] args) {
+
+
+    }
+
+
 
 
 }
